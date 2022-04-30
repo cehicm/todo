8d040bc261b8b617d432c7512a181f74cbcd71f0
Marija Cehic <marijac@stuntcoders.com>
2022-04-30 19:25:02 +0200

    Add ability to add to do item

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";var todoInput=document.querySelector(".todo__input");todoInput.addEventListener("keypress",(function(t){var e=document.querySelector(".todo__list");13===t.keyCode&&(t.preventDefault(),e.innerHTML+='<li class="todo__list__item"> '.concat(todoInput.value," </li>"),console.log(todoInput.value))}));
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,16 @@
 // Add, remove, edit a to do
 // See the number of total to dos
 // Mark as complete, move to complete
+
+const todoInput = document.querySelector(".todo__input");
+
+todoInput.addEventListener("keypress", function (e) {
+	const todoOutput = document.querySelector(".todo__list");
+
+	if (e.keyCode === 13) {
+		e.preventDefault();
+		todoOutput.innerHTML += `<li class="todo__list__item"> ${todoInput.value} </li>`;
+
+		console.log(todoInput.value);
+	}
+});
